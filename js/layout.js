77fbc0f88b57dc8647425da09f5e3f7bcc56b0bf
timschreiber <timschreiber@users.noreply.github.com>
2015-01-21 21:43:14 -0500

    tags dammit

--- js/layout.js
+++ js/layout.js
@@ -10,8 +10,11 @@ function handleTag() {
 	console.log(idx);
 	if (idx > -1) {
 		var tag = url.substring(idx + 1);
+		console.log(tag);
 		var el = $("div#" + tag + ".tag-list-item");
+		console.log(el);
 		var others = el.siblings("div.tag-list-item");
+		console.log(others);
 		el.show();
 		others.hide();
 	}
