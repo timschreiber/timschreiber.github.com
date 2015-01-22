5f9cb1c633e10ca7d552548da1260249cc7eb6f4
timschreiber <timschreiber@users.noreply.github.com>
2015-01-21 21:21:50 -0500

    tags

--- js/layout.js
+++ js/layout.js
@@ -1,3 +1,13 @@
 $(function(){
 	$("a[href^='http://'], a[href^='https://']").attr("target", "_blank");
+	handleTag();
 });
+
+function handleTag() {
+	var url = document.URL;
+	var idx = url.indexOf("#");
+	if (idx > -1) {
+		var tag = url.substring(idx + 1);
+		$(".tag-list-item").not("[id='" + tag + "']").hide();
+	}
+}
