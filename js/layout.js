3ab79221016d78bf3a1408e723ae8d29b5e3612a
timschreiber <timschreiber@users.noreply.github.com>
2015-01-21 21:35:08 -0500

    tags

--- js/layout.js
+++ js/layout.js
@@ -9,10 +9,14 @@ function handleTag() {
 	var idx = url.indexOf("#");
 	console.log(idx);
 	if (idx > -1) {
+		$("#div.tag-list-item").hide();
+	
 		var tag = url.substring(idx + 1);
 		console.log(tag);
-		var el = $("div.tag-list-item[id!='" + tag + "']");
+		var sel = "div.tag-list-item[id='" + tag + "']";
+		console.log(sel);
+		var el = $(sel);
 		console.log(el);
-		el.hide();
+		el.show();
 	}
 }
