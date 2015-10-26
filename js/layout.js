a17254a3dff913ca397dd3d0fefa4d3f12789247
Tim Schreiber <timschreiber@users.noreply.github.com>
2015-10-25 21:53:43 -0400

    updated layout.js and layout.html

--- js/layout.js
+++ js/layout.js
@@ -1,25 +1,38 @@
 $(function(){
 	$("#btnSubmit").click(function(){
-		var data = {
-			sender: $("#Sender").val(),
-			subject: $("#Subject").val(),
-			message: $("#Message").val(),
-			copySender: $("#CopySender").is(":checked"),
-			reCaptchaResponse: $("#g-recaptcha-response").val()
-		};
-		console.log(data);
-		
-		$.ajax({
-			url: "http://timschreiber.azurewebsites.net/api/contact",
-			type: "POST",
-			dataType: "json",
-			jsonp: false,
-			data: data
-		}).done(function(data){
-			console.log(data);
-		}).fail(function(data){
-			console.log(data);
-		});
+		var valid = true;
+		if(!$("#Sender").valid()) {
+			$("#errMsgs > ul").append("<li>Your email address is missing or invalid.</li>");
+			valid = false;
+		}
+		if(!$("#Subject").valid()) {
+			$("#errMsgs > ul").append("<li>Subject is required.</li>");
+			valid = false;
+		}
+		if(!$("#Message").valid()) {
+			$("#errMsgs > ul").append("<li>Message is required.</li>");
+			valid = false;
+		}
+		if(valid == true) {
+			var data = {
+				sender: $("#Sender").val(),
+				subject: $("#Subject").val(),
+				message: $("#Message").val(),
+				copySender: $("#CopySender").is(":checked"),
+				reCaptchaResponse: $("#g-recaptcha-response").val()
+			};
+			$.ajax({
+				url: "http://timschreiber.azurewebsites.net/api/contact",
+				type: "POST",
+				dataType: "json",
+				jsonp: false,
+				data: data
+			}).done(function(data){
+				console.log(data);
+			}).fail(function(data){
+				console.log(data);
+			});
+		}
 	});
 	
 	$("a[href^='http://'], a[href^='https://']").attr("target", "_blank");
@@ -28,9 +41,7 @@ $(function(){
 
 function handleTag() {
 	var url = document.URL;
-	console.log(url);
 	var idx = url.indexOf("#");
-	console.log(idx);
 	if (idx > -1) {
 		var tag = url.substring(idx + 1);
 		var el = $("div#" + tag + ".tag-list-item").show();
