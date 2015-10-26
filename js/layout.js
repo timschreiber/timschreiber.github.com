d6a27c1fd8cd1cd9919b0df418244554f0732eb8
Tim Schreiber <timschreiber@users.noreply.github.com>
2015-10-25 20:48:03 -0400

    updated layout.js and layout.html

--- js/layout.js
+++ js/layout.js
@@ -1,11 +1,25 @@
 $(function(){
-	$("#contactForm").submit(function(event){
-		event.preventDefault();
-		event.stopPropagation();
-		alert($("#contactForm").attr["action"] + "\n" + $("#contactForm").attr["method"]);
+	$("a[href^='http://'], a[href^='https://']").attr("target", "_blank");
+	handleTag();
+});
+
+function handleTag() {
+	var url = document.URL;
+	console.log(url);
+	var idx = url.indexOf("#");
+	console.log(idx);
+	if (idx > -1) {
+		var tag = url.substring(idx + 1);
+		var el = $("div#" + tag + ".tag-list-item").show();
+		el.siblings("div.tag-list-item").hide();
+	}
+}
+
+function submitContactForm(el) {
+		alert($(el).attr["action"] + "\n" + $(el).attr["method"]);
 		$.ajax({
-			url: $("#contactForm").attr["action"],
-			type: $("#contactForm").attr["method"]),
+			url: $(el).attr["action"],
+			type: $(el).attr["method"]),
 			dataType: "json",
 			jsonp: false,
 			data: {
@@ -20,21 +34,4 @@ $(function(){
 		}).fail(function(data){
 			console.log(data);
 		});
-	});
-	
-	$("a[href^='http://'], a[href^='https://']").attr("target", "_blank");
-	
-	handleTag();
-});
-
-function handleTag() {
-	var url = document.URL;
-	console.log(url);
-	var idx = url.indexOf("#");
-	console.log(idx);
-	if (idx > -1) {
-		var tag = url.substring(idx + 1);
-		var el = $("div#" + tag + ".tag-list-item").show();
-		el.siblings("div.tag-list-item").hide();
-	}
 }
