bd3905c0a3095f31ec0900699ccf7c327a53697b
Tim Schreiber <timschreiber@users.noreply.github.com>
2016-09-30 14:01:36 -0400

    js for blog index

--- js/blog2.js
+++ js/blog2.js
@@ -7,7 +7,7 @@ $(function(){
 	$("#btnTags").click(function(){
 		var tagsBtn = $("#btnTags");
 		var tagsBody = $("#tags-panel-body");
-		if(tagsBody).is(":visible") {
+		if(tagsBody.is(":visible")) {
 			tagsBody.slideUp();
 			tagsBtn.find(".fa").removeClass("fa-chevron-up").addClass("fa-chevron.down");
 		} else {
