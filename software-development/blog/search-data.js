31538a3677ba18f0e5969d0ece6238bb27af338b
Tim Schreiber <timschreiber@users.noreply.github.com>
2022-09-09 22:10:29 -0400

    Update and rename search.json to search-data.js

--- software-development/blog/search-data.js
+++ software-development/blog/search-data.js
@@ -3,7 +3,7 @@ layout: null
 ---
 {% capture newline %}
 {% endcapture %}
-[
+const searchData = [
 {% for post in site.posts %}
     {% assign newArray = '' | split: '' %}
     {% assign words = post.content | strip_html | replace: newline, " " | replace: '"', " " | replace: "\", " " | replace: "…", " " | replace: ":", " " | replace: ";", " " | replace: ",", " " | replace: ".", " " | replace: "-", " " | replace: "“": " " | replace: "”", " " | replace: "?", " " | replace: "!", " " | replace: "’", "'" | replace: "_", " " | replace: "–", " " | replace: "/", " " | split: " " %}
@@ -20,4 +20,4 @@ layout: null
         "date": "{{ post.date | date: "%a, %d %b %Y %H:%M:%S %z" }}"
     } {% unless forloop.last %},{% endunless %}
 {% endfor %}
-]
+];
